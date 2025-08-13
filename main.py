60a25e516c3474e9146d8acfe7822bfcb57beaf4
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-08-13 06:23:14 +0200

    Add initial implementation for web scraping Amazon prices

---
+++ main.py
@@ -0,0 +1,16 @@
+# 47 Amazon Prices
+
+import requests
+from bs4 import BeautifulSoup
+
+WEB_URL = "https://appbrewery.github.io/instant_pot/"
+
+HTTP_HEADER = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
+}
+
+response = requests.get(url=WEB_URL, headers=HTTP_HEADER)
+html = response.text
+html_data = BeautifulSoup(html, features="html.parser")
+
